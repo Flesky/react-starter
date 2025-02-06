8ea4be0f351517c76ea4ea15cb54d5d88d9c7a97
Baron <qbmrodrigo@gmail.com>
2025-02-07 03:48:12 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# react-starter
+
+[![code style](https://antfu.me/badge-code-style.svg)](https://github.com/antfu/eslint-config)
+
+* React 19
+* Mantine
+* Tailwind CSS
+
+To add:
+* Tailwind linting
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { MantineProvider } from '@mantine/core'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './pages/app.tsx'
+import '@mantine/core/styles.css'
+import './main.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <MantineProvider>
+      <App />
+    </MantineProvider>
+  </StrictMode>,
+)
