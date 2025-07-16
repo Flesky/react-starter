8fbb19b86653754e65ba770e53417f5359d165bb
baron-tellycom <baron@tellycom.io>
2025-07-16 12:19:42 +0800

    Add Tailwind linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,26 @@
 import antfu from '@antfu/eslint-config'
+import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss'
 
-export default antfu()
+export default antfu({ react: true }, [
+  {
+    files: ['**/*.{jsx,tsx}'],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      'better-tailwindcss': eslintPluginBetterTailwindcss,
+    },
+    rules: {
+      ...eslintPluginBetterTailwindcss.configs['recommended-error'].rules,
+    },
+    settings: {
+      'better-tailwindcss': {
+        entryPoint: 'src/main.css',
+      },
+    },
+  },
+])
